3e439aba65181521ff69219c5a4acdfde05b9a20
keigokudo <keigokudo55@googlemail.com>
2025-08-08 19:04:43 +0100

    clean up unused import

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-
-import Button from "./Button/Button";
 import "./header.css";
 
 type User = {
@@ -14,12 +11,7 @@ export interface HeaderProps {
   onCreateAccount?: () => void;
 }
 
-export const Header = ({
-  user,
-  onLogin,
-  onLogout,
-  onCreateAccount,
-}: HeaderProps) => (
+export const Header = ({}: HeaderProps) => (
   <header>
     <div className="storybook-header">
       <div>
@@ -45,26 +37,6 @@ export const Header = ({
           </g>
         </svg>
         <h1>Acme</h1>
-      </div>
-      <div>
-        {user ? (
-          <>
-            <span className="welcome">
-              Welcome, <b>{user.name}</b>!
-            </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
-          </>
-        ) : (
-          <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button
-              primary
-              size="small"
-              onClick={onCreateAccount}
-              label="Sign up"
-            />
-          </>
-        )}
       </div>
     </div>
   </header>
