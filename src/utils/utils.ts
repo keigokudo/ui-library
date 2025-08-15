daed2a94b076a50460d1528387d8f2d7de72efc7
keigokudo <keigokudo55@googlemail.com>
2025-08-15 15:59:18 +0100

    add object support for clsx

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,12 @@
+type ClassValue =
+  | string
+  | number
+  | boolean
+  | undefined
+  | null
+  | Record<string, any>
+  | ClassValue[];
+
 /**
  * Combines multiple class names into a single string, handling arrays and filtering out falsy values.
  * Useful for dynamically constructing CSS class strings in React or other frameworks.
@@ -7,10 +16,19 @@
  * @example
  * clsx("btn", undefined, "active", ["text-bold", "p-2"]) // Returns "btn active text-bold p-2"
  */
-type ClsxValue = string | boolean | undefined | null | ClsxValue[];
-export function clsx(...classes: ClsxValue[]) {
+
+export function clsx(...classes: ClassValue[]): string {
   return classes
-    .flat() // Flattens nested arrays into a single array
+    .flatMap(processClassValue) // Flattens nested arrays into a single array
     .filter(Boolean) // Removes falsy values (e.g., undefined, null, false)
     .join(" "); // Joins class names with a space
+}
+
+function processClassValue(cls: ClassValue): ClassValue {
+  if (typeof cls === "object" && cls != null && !Array.isArray(cls)) {
+    return Object.entries(cls)
+      .filter(([_, value]) => Boolean(value)) // Filters out entries with falsy values
+      .map(([key]) => key);
+  }
+  return cls;
 }
